56c4764f9e777b8e36862ea9c5cdfe099b2f3f65
Skullhead200 <71871501+Skullhead200@users.noreply.github.com>
2020-10-07 21:14:35 +0530

    1_Hello_World.dart

---
+++ 1_Hello_World.dart
@@ -0,0 +1,18 @@
+void main()
+ {
+
+  // This is my first line of code.
+
+print("Hello World");  // this is another comment
+
+print("This is my first application");
+
+// Performing arithematic operation
+
+print(40/10);
+
+// Printing out boolean value
+
+print(true);
+
+}
